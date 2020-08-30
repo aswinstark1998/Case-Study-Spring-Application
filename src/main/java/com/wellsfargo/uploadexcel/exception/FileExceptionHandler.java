4aab5608eda1502ccd7b292335a101d742e3e12f
Aswin A <asweinnovate@gmail.com>
2020-08-30 22:19:07 +0530

    Added ExceptionHandler class to deny file upload if size is large.

--- src/main/java/com/wellsfargo/uploadexcel/exception/FileExceptionHandler.java
+++ src/main/java/com/wellsfargo/uploadexcel/exception/FileExceptionHandler.java
@@ -1,5 +1,19 @@
 package com.wellsfargo.uploadexcel.exception;
 
-public class FileExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.wellsfargo.uploadexcel.restcontroller.ResponseMessage;
+
+@ControllerAdvice
+public class FileExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	  @ExceptionHandler(MaxUploadSizeExceededException.class)
+	  public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+	    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
+	  }
 }
